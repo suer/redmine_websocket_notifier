582bc5d4608f893cde5329814f7a3b5395bd0da8
suer <sue@nagoya-u.jp>
2011-07-17 21:35:57 +0900

    use queue

--- lib/websocket_server.rb
+++ lib/websocket_server.rb
@@ -14,19 +14,12 @@ class WebsocketServer
   end
 
   def send_message(message, logger)
-    logger.info  @connection_queue.size
     until @connection_queue.empty?
-      con = @conection_queue.pop
+      con = @connection_queue.pop
       @connections << con unless @connections.include?(con)
     end
-    logger.info "*****************"
-    logger.info message
-    logger.info "*****************"
-    logger.info  @connections.size
-    logger.info  @messages
     @messages.push(message)
-    #@@connections.each {|con| con.send(msg) unless con == ws} 
-    #@@connections.each {|con| con.send(msg) } 
+    @connections.each {|con| con.send(message) } 
   end
 
   private
@@ -35,9 +28,6 @@ class WebsocketServer
       EventMachine::WebSocket.start(:host => "127.0.0.1", :port => 18082, :debug => true) do |ws|
         ws.onopen { 
           @connection_queue << ws
-          puts "================================"
-          puts @connection_queue.size
-          puts "================================"
           @messages.each {|prev| ws.send(prev)}
         }
 
@@ -48,9 +38,6 @@ class WebsocketServer
         }
 
         ws.onclose {
-          puts "================================"
-          puts "close!!!!!!!!!!!!!!!"
-          puts "================================"
           @connections.delete_if{|con| con == ws} 
         }
       end
