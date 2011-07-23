4a6d03f80ad3e269f0503e2c337f30be28f837a4
suer <sue@nagoya-u.jp>
2011-07-24 03:50:42 +0900

    configurable

--- lib/websocket_notifier_patch.rb
+++ lib/websocket_notifier_patch.rb
@@ -1,18 +1,36 @@
 require "open-uri"
 module WebsocketNotifierPatch
+  CONFIG_PATH = File.dirname(__FILE__) + '/../config/websocket.yml'
+  DEFAULT_WEBSOCKET_CONFIG = {'websocket' => {'host' => '127.0.0.1', 'redmine_side_port' => 4567, 'client_side_port' => 18082, 'use_ssl' => false}}
+  config = nil
+  if File.exists?(CONFIG_PATH)
+    open(CONFIG_PATH) {|f| config = YAML::load(f.read)}
+  end
+  LOADED_WEBSOCKET_CONFIG = config
+
   def self.included(base)
     base.send(:include, InstanceMethods)
     base.class_eval do
       unloadable
       after_create :send_message    
-      puts self.class
-      cattr_accessor :websocket_server
+      cattr_accessor :websocket_config
+      @@websocket_config = LOADED_WEBSOCKET_CONFIG || DEFAULT_WEBSOCKET_CONFIG
+      @@websocket_config['websocket'] = DEFAULT_WEBSOCKET_CONFIG['websocket'].merge(@@websocket_config['websocket'])
     end
   end
   module InstanceMethods
     def send_message
+      protocol = self.class.websocket_config['websocket']['use_ssl'] ? 'https' : 'http'
+      host = self.class.websocket_config['websocket']['host'] || '127.0.0.1'
+      port = self.class.websocket_config['websocket']['redmine_side_port'] || '4567'
       message = "#{event_title}\n#{event_description}"
-      open("http://localhost:4567/message?activity=#{URI.escape(message)}")
+      url = "#{protocol}://#{host}:#{port}/?message=#{URI.escape(message)}"
+      begin
+        open(url)
+      rescue
+        # ignore connection error
+        logger.warn "cannot open websocket server: #{url}"
+      end
     end
   end
 end
--- lib/websocket_server.rb
+++ lib/websocket_server.rb
@@ -2,29 +2,51 @@ require 'rubygems'
 require 'em-websocket'
 require 'sinatra'
 
-class WebsocketServer
-  @@connections = []
+module WebsocketNotifier
+  class WebsocketServer
+    CONFIG_PATH = File.dirname(__FILE__) + '/../config/websocket.yml'
+    DEFAULT_CONFIG = {'websocket' => {'redmine_side_port' => 4567, 'client_side_port' => 18082}}
+    @@connections = []
+    @@config = nil
 
-  def start_websocket_server
-    EventMachine::WebSocket.start(:host => "127.0.0.1", :port => 18082, :debug => true) do |ws|
-      ws.onopen { @@connections << ws }
+    if File.exists?(CONFIG_PATH)
+      open(CONFIG_PATH) {|f| @@config = YAML::load(f.read)}
     end
-  end
+    @@config ||= DEFAULT_CONFIG
+    @@config['websocket'] = DEFAULT_CONFIG['websocket'].merge(@@config['websocket'])
+    def start_websocket_server
+      port = @@config['websocket']['client_side_port']
+      EventMachine::WebSocket.start(:host => "0.0.0.0", :port => port) do |connection|
+        connection.onopen { @@connections << connection }
+        connection.onmessage { }
+        connection.onclose { @@connections.delete connection }
+      end
+    end
 
-  def self.notify_message(message)
-    @@connections.each {|con| con.send(message)} 
-  end
+    def self.notify_message(message)
+      @@connections.each {|con| con.send(message)} 
+    end
 
-  def start_web_server
-    get '/message' do
-      WebsocketServer::notify_message params[:activity]
-      ""
+    def self.config
+      @@config
+    end
+
+    def start_web_server
+      config = WebsocketNotifier::WebsocketServer::config
+      port = config['websocket']['redmine_side_port']
+      configure do
+        set :port, port
+      end
+      get '/' do
+        WebsocketNotifier::WebsocketServer::notify_message params[:message]
+        ""
+      end
     end
   end
 end
 
 if __FILE__ == $0
-  server = WebsocketServer.new
+  server = WebsocketNotifier::WebsocketServer.new
   Thread.start do
     server.start_websocket_server
   end
